a3c5c695981f9382507c25f3630d162f3b04b1f5
Amaaz Arshad <amaaz992000@gmail.com>
2022-08-06 12:53:40 +0500

    vercel.json file corrected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const db = require("../database");
+const db = require("./database");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
