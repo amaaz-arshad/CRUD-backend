dd8630be7e68fcc921ae8330a61d16473733e8cf
Amaaz Arshad <amaaz992000@gmail.com>
2022-08-06 12:36:24 +0500

    vercel config added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const db = require("./database");
 const bodyParser = require("body-parser");
 const cors = require("cors");
